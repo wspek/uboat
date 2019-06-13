52318a9a220459657f351020aa9d0b4629c4457e
Waldo Spek <waldospek@gmail.com>
2019-06-13 19:00:09 -0300

    Bugfix (minor)

--- apps/fetch/static/fetch/js/fetch.js
+++ apps/fetch/static/fetch/js/fetch.js
@@ -128,7 +128,7 @@ function fetchAndDisplaySubtitles() {
 
     var availableLanguages = {};
     getAvailableLanguages(function(languages) {
-        this.availableLanguages = languages;
+        availableLanguages = languages;
     })
 
     // This function is called when we successfully retrieve the subtitle data
