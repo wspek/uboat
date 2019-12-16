5412d4da257cfa15ce2096f4e1136f89095ca005
Waldo Spek <waldospek@gmail.com>
2019-12-16 18:51:12 -0300

    Change OpenSubtitles officially registered UserAgent

--- apps/sink/config.py
+++ apps/sink/config.py
@@ -1,6 +1,4 @@
 API_URL = 'http://api.opensubtitles.org/xml-rpc'
-USER_AGENT = 'TemporaryUserAgent'
-USER = 'batchsubs'
-PASSWD = 'madabamiti'
+USER_AGENT = 'uboat'
 STORAGE_PATH = "/home/wspek/Development/python/django_projects/uboat/test/output"
 LANGUAGES_CSV = 'apps/sink/static/sink/config/opensubtitles/languages_enabled.csv'
